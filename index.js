ef6000e9e9765d905fd109a1d2f8b305fcd32c7d
Dominique Skaf <doskaf@yahoo.com>
2020-03-09 20:35:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function BoardMember(name, homeState, training) {
   this.homeState = homeState;
   this.training = training;
 }
+
+BoardMember.prototype.veto = function() {
+  return "No, I must disagree"
+}
+
+BoardMember.prototype.approve = function() {
+  return "You can do that!"
+}
+
